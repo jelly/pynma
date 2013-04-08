1c466c9ea77f6024ef5aec89c063440660bfd8ca
Jelle van der Waa <jelle@vdwaa.nl>
2013-04-08 15:58:01 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup (
+        name='pynma',
+        version='0.1',
+        description='Pynma is a simple python module for the NotifyMyAndroid API',
+        keywords='',
+        author='Jelle van der Waa',
+        author_email='jelle@vdwaa.nl',
+        url='https://github.com/jelly/pynma',
+        license='GNU GPLv3',
+        packages=['pynma'],
+        install_requires=[
+        ],
+        classifiers=[
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
